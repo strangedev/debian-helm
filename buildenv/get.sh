1d4c7f9d9fa379767d9063826fa1244619962930
Noah Hummel <n.p.hummel@gmail.com>
2019-03-26 16:25:16 +0100

    [FIX] unpack tar.gz

--- buildenv/get.sh
+++ buildenv/get.sh
@@ -6,8 +6,23 @@ ARTEFACT_REPO_URL="https://storage.googleapis.com/kubernetes-helm"
 get_release_url()
 {
 	version=$1
-	architecture=$2
+	architecture=$(get_helm_architecture_name $2)
+
 	echo "${ARTEFACT_REPO_URL}/helm-v${version}-linux-${architecture}.tar.gz"
+}
+
+get_helm_architecture_name()
+{
+	# upstream drops the i in i386 for some reason
+	# case used here in case this worsens in the future
+	case $1 in
+		"i386" )
+			echo "386"
+			;;
+		* )
+			echo "$1"
+			;;
+	esac
 }
 
 VERSION=$1
@@ -24,7 +39,8 @@ for architecture in $ARCHITECTURES; do
 	sed -i "s/{{VERSION}}/${versionEscaped}/g" "${architecture}/DEBIAN/control"
 	sed -i "s/{{ARCHITECTURE}}/${architecture}/g" "${architecture}/DEBIAN/control"
 
-	echo $(get_release_url "$VERSION" "$architecture")
 	curl $(get_release_url "$VERSION" "$architecture") \
-		-o "./${architecture}/usr/local/bin/helm"
+		| tar -xz \
+		-C "./${architecture}/usr/local/bin/" \
+		"linux-$(get_helm_architecture_name $architecture)/helm" --strip-components=1
 done
